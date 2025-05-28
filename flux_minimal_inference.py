7b289a98ec6cd235f51a55c92a3a68ddb7ab5198
ilaykid <ilaykid@gmail.com>
2025-05-28 09:36:33 +0000

    fix cancellation logging in denoise function to avoid using missing variables

--- flux_minimal_inference.py
+++ flux_minimal_inference.py
@@ -104,11 +104,10 @@ def denoise(
         b_txt = txt
         b_vec = vec
         b_t5_attn_mask = t5_attn_mask
-
-    for i, (t_curr, t_prev) in enumerate(zip(tqdm(timesteps[:-1]), timesteps[1:])):
-        if cancel_flag is not None and len(cancel_flag)>0:
-            logger.info(f"Operation cancelled at step {i}/{len(timesteps)-1}.")
-            return None # return None to indicate cancellation
+        for t_curr, t_prev in zip(tqdm(timesteps[:-1]), timesteps[1:]):
+            if cancel_flag is not None and len(cancel_flag)>0:
+                logger.info(f"Operation cancelled")
+                return None # return None to indicate cancellation
         t_vec = torch.full(
             (b_img_ids.shape[0],), t_curr, dtype=img.dtype, device=img.device
         )
